fe5e2704246e0d9b00cea3b49a6b64aa2402e496
Crowdbotics <team@crowdbotics.com>
2019-04-11 05:00:26 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT bp_test_2154.wsgi:application
---
+++ bp_test_2154/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bp_test_2154 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bp_test_2154.settings")
+
+application = get_wsgi_application()
